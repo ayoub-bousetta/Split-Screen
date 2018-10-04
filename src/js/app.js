973897933fab085ead56ff711f8f3267a4e3c945
Aubbusta <aubbusta@gmail.com>
2018-10-05 00:13:01 +0100

    Split screen

---
+++ src/js/app.js
@@ -0,0 +1,63 @@
+const interact = require('interactjs');
+
+    let wrapper = document.getElementById('waprer');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    
+  let delta=0;
+  
+
+      
+    
+     //  handle.style.left = e.clientX + delta + 'px';
+      
+    
+      // topLayer.style.width= e.clientX  + delta + 'px';
+     
+  
+
+
+
+  interact('.handle')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+   
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target;
+   
+    // keep the dragged position in the data-x/data-y attributes
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+// translate the element
+target.style.webkitTransform =
+target.style.transform =
+  'translate(' + x + 'px, ' + y + 'px)';
+
+
+
+topLayer.style.width= event.clientX   + 'px';
+
+// update the posiion attributes
+target.setAttribute('data-x', x);
+target.setAttribute('data-y', y);
+  
+ 
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
